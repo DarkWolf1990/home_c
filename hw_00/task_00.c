4bb1c36cd8db5a2cfc4a434853b000e1b7a1393c
darkwolf1990 <whitejaguar08@gmail.com>
2025-02-03 22:12:23 +0300

    solution home_work

---
+++ hw_00/task_00.c
@@ -0,0 +1,11 @@
+//
+// Created by admin on 03.02.2025.
+//
+#include <stdio.h>
+
+
+int main(){
+
+printf("Hello World!");
+    return 0;
+}
---
+++ hw_03/task_03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int number_1, nmber_2, number_3, result;
+    scanf("%d %d %d",&number_1, &nmber_2, &number_3 );
+    result = number_1+nmber_2+number_3;
+    printf("%d+%d+%d=%d",number_1,nmber_2,number_3,result);
+
+    return 0;
+}
