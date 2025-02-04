ae5953d37afba27b5034d35e3c131f353db40584
darkwolf1990 <whitejaguar08@gmail.com>
2025-02-04 20:38:54 +0300

    solution 9 - 10 - 11 task

--- hw_10/task_10.c
+++ hw_10/task_10.c
@@ -3,17 +3,17 @@
 #define N 5
 
 int main() {
-    int num, max = INT_MIN;
+    int num, min = INT_MAX;
 
 
     for (int i = 0; i < N; i++) {
         scanf("%d", &num);
 
-        if (num > max) {
-            max = num;
+        if (num < min) {
+            min = num;
         }
     }
 
-    printf("%d", max);
+    printf("%d", min);
     return 0;
 }
--- hw_11/task_11.c
+++ hw_11/task_11.c
@@ -3,7 +3,7 @@
 #define N 5
 
 int main() {
-    int num, max = INT_MIN;
+    int num, max = INT_MIN, min = INT_MAX;
 
 
     for (int i = 0; i < N; i++) {
@@ -12,8 +12,11 @@ int main() {
         if (num > max) {
             max = num;
         }
+        if (num < min) {
+            min = num;
+        }
     }
 
-    printf("%d", max);
+    printf("%d", max+min);
     return 0;
 }
